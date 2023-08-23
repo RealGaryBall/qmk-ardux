932e8a8ae6413c2b0105f30f92e9485770c4b5e9
RealGaryBall <real.gary.ball@gmail.com>
2023-08-23 10:42:48 -0600

    Update config.h

--- keyboards/ardux/thepaintbrush/config.h
+++ keyboards/ardux/thepaintbrush/config.h
@@ -5,11 +5,11 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x7E71
-#define PRODUCT_ID      0x7E72
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    ardux
-#define PRODUCT         thepaintbrush
+//#define VENDOR_ID       0x7E71
+//#define PRODUCT_ID      0x7E72
+//#define DEVICE_VER      0x0001
+//#define MANUFACTURER    ardux
+//#define PRODUCT         thepaintbrush
 //#define DESCRIPTION     thepaintbrush
 
 /* Board layout */
