a32f3cc0fd810ff76a242746ee6055826a85143a
RealGaryBall <real.gary.ball@gmail.com>
2023-08-23 10:18:23 -0600

    Update config.h

--- keyboards/ardux/thepaintbrush/config.h
+++ keyboards/ardux/thepaintbrush/config.h
@@ -10,12 +10,12 @@
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    ardux
 #define PRODUCT         thepaintbrush
-#define DESCRIPTION     thepaintbrush
+//#define DESCRIPTION     thepaintbrush
 
 /* Board layout */
 #define MATRIX_ROWS 2
 #define MATRIX_COLS 4
-#define UNUSED_PINS
+//#define UNUSED_PINS
 
 /* Prevent jitters in key presses */
 #define DEBOUNCE 5
